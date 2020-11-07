804cf49759f9217a39593d1ef7cebdd6b8317044
Renante D. Entera <renantee@codev.com>
2020-11-07 16:49:42 +0800

    Angular Material: Components Demo with Routing (Updated)
    Execute commands:
    > ng g c card --skipTests true
    > ng g c datepicker --skipTests true
    > ng g c navigation --skipTests true
    > npm start

--- material-demo/src/app/app-routing.module.ts
+++ material-demo/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CardComponent } from './card/card.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 import { InputComponent } from './input/input.component';
 
 const routes: Routes = [
+  { path: 'card', component: CardComponent },
   { path: 'checkbox', component: CheckboxComponent },
+  { path: 'datepicker', component: DatepickerComponent },
   { path: 'input', component: InputComponent }
 ];
 
--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 import { InputComponent } from './input/input.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardComponent,
     CheckboxComponent,
-    InputComponent
+    DatepickerComponent,
+    InputComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
@@ -28,8 +39,13 @@ import { InputComponent } from './input/input.component';
     AppRoutingModule,
     MatCardModule,
     MatCheckboxModule,
+    MatDatepickerModule,
     MatInputModule,
-    MatRadioModule
+    MatListModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSidenavModule,    
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
