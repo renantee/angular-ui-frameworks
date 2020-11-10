762a06c356e55162dcf59ef120e7058ab531d311
Renante D. Entera <renantee@codev.com>
2020-11-10 14:47:31 +0800

    Course Project: Responsive Dashboard
    Update includes:
    > Add Angular Material
    > Add Ng2-Charts
    > Add Ng2-Charts Schematics as a Dev Dependency
    > Generate Navigation Component
    > Generate Dashboard Component
    > Generate Card Component
    
    Execute commands:
    > ng new material-dashboard
    > cd material-dashboard
    > ng add @angular/material
    > npm install ng2-charts --save
    > npm install chart.js --save
    > npm install --save-dev ng2-charts-schematics
    > ng generate @angular/material:dashboard dash
    > ng g c card -m app --style css
    > npm start

---
+++ material-dashboard/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from 'ng2-charts';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashComponent } from './dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    DashComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    ChartsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
