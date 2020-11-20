c447ab7d8d0cd32cd90c339a878534d97644a6be
Renante D. Entera <renantee@codev.com>
2020-11-20 16:07:22 +0800

    Course Project: Integrate Free Bootstrap Design (Updated)
    Update includes:
    > Update "angular.json"
    > Install RxJS, perfect-scrollbar and jquery Dependencies
    > Add Images, CSS and SCSS to Assets
    > Install sass-loader as Dev Dependency
    
    Fixing bugs:
    > `bug-cannot-find-module-admin-layout.png` = Cannot find module './layouts/admin-layout/admin-layout.module'
    > `bug-filter-does-not-exist-on-type-observable.png` = Property 'filter' does not exist on type 'Observable<Event>' (done)
    > `warning-common-js-dependencies.png` = CommonJS or AMD dependencies can cause optimization bailouts (done)
    
    Execute commands:
    > cd material-dashboard
    > npm install rxjs
    > npm install perfect-scrollbar
    > npm install jquery
    > npm install sass-loader sass webpack --save-dev
    > npm start

--- material-dashboard/src/app/app.module.ts
+++ material-dashboard/src/app/app.module.ts
@@ -9,14 +9,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
@@ -35,14 +27,6 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    DashboardComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    TableListComponent,
-    TypographyComponent,
-    UserProfileComponent,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- material-dashboard/src/app/layouts/admin-layout/admin-layout-routing.module.ts
+++ material-dashboard/src/app/layouts/admin-layout/admin-layout-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { TypographyComponent } from '../../typography/typography.component';
+import { IconsComponent } from '../../icons/icons.component';
+import { MapsComponent } from '../../maps/maps.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class AdminLayoutRoutingModule { }
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'typography',     component: TypographyComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'notifications',  component: NotificationsComponent }
+];
